bd5b092156212d8a1e3fb59f8d532d791673e4c8
lewsan <liujintong@peiwo.cn>
2017-08-15 20:27:24 +0800

    fix

--- src/peiwoadmin/urls.py
+++ src/peiwoadmin/urls.py
@@ -20,9 +20,9 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = [
-                      url(r'^__debug__/', include(debug_toolbar.urls)),
-                  ] + urlpatterns
+# if settings.DEBUG:
+#     import debug_toolbar
+#
+#     urlpatterns = [
+#                       url(r'^__debug__/', include(debug_toolbar.urls)),
+#                   ] + urlpatterns
