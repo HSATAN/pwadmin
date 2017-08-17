1f08d9bf79e42fbb6f6a13eb7aa73a7db49eb903
yanzhao <649038269@qq.com>
2017-08-17 15:38:20 +0800

    SDK for sneaky admin/account

---
+++ src/admin_interface/account.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+class Account(object):
+    VERIFYUSER = '/admin/account/verifyuser'
+
+    def __init__(self, host='http://172.16.10.134:9090'):
+        self.host = host
+
+
+    def generate_url(self, url):
+        return "{}{}".format(self.host, url)
+
+    def verifyuser(self, uid, password):
+        url = self.generate_url(self.VERIFYUSER)
+        resp = requests.post(url, data={"uid": uid, 'password': password})
+        return json.loads(resp.content)
