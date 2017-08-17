8ed84a3c58a5c8401134719d31e5c0e678ff3434
yanzhao <649038269@qq.com>
2017-08-17 17:51:32 +0800

    Updated task template

--- src/pwadmin/urls.py
+++ src/pwadmin/urls.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.conf.urls import url
-from .views import pwmanager, index
+from .views import pwmanager, index, task
 
 urlpatterns = [
     url(r'^$', index.Index.as_view(), name='home'),
     url(r'^sign-in/$', pwmanager.SignIn.as_view(), name='sign-in'),
     url(r'^logout/$', pwmanager.LogoutView.as_view(), name='logout'),
+    url(r'^task/$', task.Task.as_view(), name='task'),
 ]
