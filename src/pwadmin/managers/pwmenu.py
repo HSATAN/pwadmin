52614532dd4633e635425f524c38206bf6ac5b8c
yanzhao <649038269@qq.com>
2017-08-25 20:45:40 +0800

    Update menu-tree api

--- src/pwadmin/managers/pwmenu.py
+++ src/pwadmin/managers/pwmenu.py
@@ -10,8 +10,24 @@ class PwMenuManager(Manager):
         return self.annotate(no_len=Length('no')).filter(no_len=2).order_by(order_by)
 
     def tree(self, order_by='no'):
+        """
+
+        Args:
+            order_by:
+
+        Returns(dict):
+           {tree_self_key(): {},
+            tree_children_key(): {},
+           }
+
+        """
         children = {}
         first_level_menu = self.first_level_menu(order_by)
         for i in first_level_menu:
-            children.update(i.tree(order_by))
-        return children
+            children.update({i.tree_id_key: i.tree(order_by)})
+        return {
+            self.model.tree_self_key(): {'id': 'menu',
+                                         'name': '菜单',
+                                         'checked': False},
+            self.model.tree_children_key(): children
+        }
--- src/pwadmin/models/permissions.py
+++ src/pwadmin/models/permissions.py
@@ -34,30 +34,28 @@ class PwMenu(models.Model):
 
         Returns(dict):
           - menu_tree: -
-            {tree_id_key: {
+            {
                     tree_self_key: tree_self_content,
                     tree_children_key: {
                     }
             }
         """
         if not self.all_level_children().exists():
-            return {self.tree_id_key: {self.tree_self_key: self.tree_self_content,
-                                       self.tree_children_key: {}}}
+            return {self.tree_self_key(): self.tree_self_content,
+                    self.tree_children_key(): {}}
         children = {}
         for child in self.all_level_children(order_by):
-            children.update(child.tree(order_by))
-        return {
-            self.tree_id_key: {
-                self.tree_self_key: self.tree_self_content,
-                self.tree_children_key: children}
-        }
+            children.update({child.tree_id_key: child.tree(order_by)})
+        return {self.tree_self_key(): self.tree_self_content,
+                self.tree_children_key(): children}
+
 
     @property
     def tree_id_key(self):
         return str(self.no)
 
-    @property
-    def tree_self_key(self):
+    @staticmethod
+    def tree_self_key():
         return 'node'
 
     @property
@@ -66,8 +64,8 @@ class PwMenu(models.Model):
                 'name': self.name,
                 'checked': False}
 
-    @property
-    def tree_children_key(self):
+    @staticmethod
+    def tree_children_key():
         return 'children'
 
     def __str__(self):
