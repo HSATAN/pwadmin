a035aba9758347e2b3512ed4a9f3c3c27d6948ec
yanzhao <649038269@qq.com>
2017-08-25 11:57:57 +0800

    Update menu-tree.jsx

--- src/pwadmin/static/pwadmin/permissions/menu-tree.jsx
+++ src/pwadmin/static/pwadmin/permissions/menu-tree.jsx
@@ -10,7 +10,11 @@ class Tree extends React.Component {
      *
      * @param props - {node_key: str
      *                 node_children_key: str
-     *                 tree: array
+     *                 tree: [
+     *                     {node_key: {id:, name: ''},
+     *                      node_children_key: tree
+     *                     },
+      *                     ...]
      *                 }
      */
     constructor(props) {
@@ -21,7 +25,38 @@ class Tree extends React.Component {
         this.renderNode = this.renderNode.bind(this);
         this.node_key = this.node_key.bind(this);
         this.node_children_key = this.node_children_key.bind(this);
+        this.handleSymbol = this.handleSymbol.bind(this);
+        this.get_or_create_symbol_value = this.get_or_create_symbol_value.bind(this);
+        this.get_symbol_key = this.get_symbol_key.bind(this);
     }
+
+    handleSymbol(e) {
+        const $target = $(e.target);
+        const target_id = $target.data('id');
+        let symbol_key = this.get_symbol_key(target_id);
+        let symbol = this.get_or_create_symbol_value(target_id);
+        symbol = !symbol;
+        this.setState({
+            [symbol_key]: symbol
+        })
+    }
+
+    get_symbol_key(i) {
+        return 'symbol_' + i;
+    }
+
+    get_or_create_symbol_value(i) {
+        const symbol_key = this.get_symbol_key(i);
+        let symbol = this.state[symbol_key];
+        if (symbol === undefined) {
+            this.setState({
+                [symbol_key]: false
+            });
+            return false;
+        }
+        return symbol;
+    }
+
 
     node_key() {
         return this.props.node_key || 'node';
@@ -36,14 +71,22 @@ class Tree extends React.Component {
         const content = node[this.node_key()];
 
         if ($.isEmptyObject(children)) {
-            return <a className="list-group-item">{content.name}[{content.id}]</a>
+            return <a className="list-group-item" key={content.id}>
+                <input type="checkbox" aria-label="Checkbox for following text input"/>
+                {content.name}[{content.id}]</a>
         }
-        return <div>
-            <a className="list-group-item"
-               data-toggle="collapse"
-               href={"#" + content.id}
-               aria-expanded="false"
-               aria-controls="collapseExample">
+        return <div key={content.id}>
+            <a className="list-group-item">
+                <i className={this.get_or_create_symbol_value(content.id) ? "fa fa-minus" : "fa fa-plus"}
+                   aria-hidden="true"
+                   data-id={content.id}
+                   onClick={this.handleSymbol}
+                   data-toggle="collapse"
+                   href={"#" + content.id}
+                   aria-expanded="false"
+                   aria-controls="collapseExample"
+                ></i>
+                <input type="checkbox" aria-label="Checkbox for following text input"/>
                 {content.name}[{content.id}]
             </a>
             <div className="collapse" id={content.id}>
