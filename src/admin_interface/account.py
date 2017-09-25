dca2726d5af4ae75088a56a9d03d2000f334e724
yanzhao <649038269@qq.com>
2017-09-25 16:58:46 +0800

    Update

--- src/admin_interface/account.py
+++ src/admin_interface/account.py
@@ -1,12 +1,12 @@
 import requests
 import json
 
+
 class Account(object):
     VERIFYUSER = '/admin/account/verifyuser'
 
     def __init__(self, host='http://172.16.10.134:9090'):
         self.host = host
-
 
     def generate_url(self, url):
         return "{}{}".format(self.host, url)
