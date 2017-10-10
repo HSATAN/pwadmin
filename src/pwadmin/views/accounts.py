bcc4ccaf9dceee531c4ede4986d5c777c55c2293
yanzhao <649038269@qq.com>
2017-10-10 10:21:43 +0800

    Update

--- src/pwadmin/views/accounts.py
+++ src/pwadmin/views/accounts.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
+import requests
+from django.conf import settings
 from django.shortcuts import render
 from django.views.generic import View
+from pwadmin.forms.accounts import SignUpSearchForm
+from utils.sdk import sneakSDK
 
 class Accounts(View):
+    """用户-基础信息-注册用户.
+    """
     template = 'pwadmin/accounts/register_user.html'
+    account = sneakSDK.account
+
     def get(self, request, *args, **kwargs):
+        form = SignUpSearchForm()
+        data = self.account.query()
         return render(request, self.template)
+
+    def post(self, request, *args, **kwargs):
+        form = SignUpSearchForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            result = self.account.query(**data)
+        return render(request, self.template, context={})
