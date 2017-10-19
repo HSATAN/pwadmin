df28d1b455cb8c026e035b72432734f1999035c1
yanzhao <649038269@qq.com>
2017-10-19 18:56:16 +0800

    修正了同一filter多次请求的问题

--- src/pwadmin/static/pwadmin/list-tpl.jsx
+++ src/pwadmin/static/pwadmin/list-tpl.jsx
@@ -1,53 +1,35 @@
 import React from 'react';
-import {observable, computed, reaction, action} from "mobx";
+import {observer} from 'mobx-react';
 
-class SearchStore {
-    @observable query = '';
-    @observable filter = [];  // array of {name: filed_name, value: ''}
-    @observable search = false; // 是否向服务器发送请求.
-    @observable result = {};  // 服务器返回的data.
-
-    constructor() {
-        reaction(() => this.data(), data => {
-            this.fetchResult(data)
-        })
+@observer
+class BaseListView extends React.Component {
+    /**
+     *
+     * @param props: search, table, filter
+     */
+    constructor(props) {
+        super(props);
     }
 
-    data() {
-        const kwargs = {
-            query: this.query
-        };
-        this.filter.map((obj, index) => {
-            if (obj.value) {
-                kwargs[obj.name] = obj.value;
-            }
-        });
-        return [this.search, kwargs];
-    }
+    render() {
+        return <div>
+            <ol className="breadcrumb">
+                <li className="breadcrumb-item"><a href="#">Home</a></li>
+                <li className="breadcrumb-item"><a href="#">用户</a></li>
+                <li className="breadcrumb-item active">注册用户</li>
+            </ol>
+            <div className="row">
+                <div className="col-9">
+                    {this.props.search}
+                    {this.props.table}
+                </div>
+                <div className="col-3">
+                    {this.props.filter}
+                </div>
+            </div>
 
-    @action
-    fetchResult(data) {
-        const search = data[0];
-        const params = data[1];
-        if (search) {
-            $.ajax({
-                url: url,
-                method: 'GET',
-                data: params
-            }).done(this.done).fail(this.fail)
-        }
-    }
-
-    @action.bound
-    done(data, status, xhr) {
-        this.result = data;
-    }
-
-    @action.bound
-    fail(qXHR, textStatus, errorThrown) {
-        alert(textStatus);
-        console.log(errorThrown);
+        </div>
     }
 }
 
-export {SearchStore}
+export {BaseListView}
