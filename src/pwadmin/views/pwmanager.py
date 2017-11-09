b7f5c5b8ae108911ce1331c08fe78f11f6c2c80b
yanzhao <649038269@qq.com>
2017-11-09 17:56:27 +0800

    Update sign in views

--- src/pwadmin/views/pwmanager.py
+++ src/pwadmin/views/pwmanager.py
@@ -2,49 +2,46 @@
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
-from django.views.generic import View
 from django.contrib.auth import authenticate, login, logout as auth_logout
 from django.contrib.auth import views as auth_view
 from django.http import JsonResponse, HttpResponseRedirect
 from pwadmin.forms.pwmanager import SignInForm
 from admin_interface.account import Account
 from django.conf import settings
+from contrib.views import BaseView
 
 
-class SignIn(View):
+class SignIn(BaseView):
     template = 'pwadmin/sign-in.html'
 
-    def get(self, request, *args, **kwargs):
+    def get_template(self, request, *args, **kwargs):
         next = request.GET.get('next', '')
         if not next:
             next = settings.LOGIN_REDIRECT_URL
         return render(request, self.template, {'next': next})
 
-    def post(self, request, *args, **kwargs):
-        if request.is_ajax():
-            form = SignInForm(data=request.POST)
-            if form.is_valid():
-                uid = form.cleaned_data['uid']
-                password = form.cleaned_data['password']
-                user = authenticate(request, uid=uid, password=password)
-                if user is not None:
-                    token = Account.login(uid, password, host=settings.API_HOST)['token']
-                    user.set_sdk(token)
-                    login(request, user)
-                    return JsonResponse({
-                        "code": 0
-                    })
+    def post_ajax(self, request, *args, **kwargs):
+        form = SignInForm(data=request.POST)
+        if form.is_valid():
+            uid = form.cleaned_data['uid']
+            password = form.cleaned_data['password']
+            user = authenticate(request, uid=uid, password=password)
+            if user is not None:
+                token = Account.login(uid, password, host=settings.API_HOST)['token']
+                user.set_sdk(token)
+                login(request, user)
                 return JsonResponse({
-                    'code': 1,
-                    'msg': '用户名或者密码错误'
+                    "code": 0
                 })
-            else:
-                return JsonResponse({
-                    "code": 1,
-                    'msg': form.errors.as_json()
-                })
+            return JsonResponse({
+                'code': 1,
+                'msg': '用户名或者密码错误'
+            })
         else:
-            pass
+            return JsonResponse({
+                "code": 1,
+                'msg': form.errors.as_json()
+            })
 
 
 class LogoutView(auth_view.LogoutView):
