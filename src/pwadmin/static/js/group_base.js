8200f10b20085d9a01901e0865451872d2b82fb2
WMT <wangmingtao@peiwo.cn>
2017-11-09 12:18:12 +0800

    navigations js

--- src/pwadmin/static/js/group_base.js
+++ src/pwadmin/static/js/group_base.js
@@ -1,24 +1,5 @@
 leftVaigation();
-
 //设置左侧导航栏颜色
-function leftVaigation() {
-    var url = window.location.href;
-    var bases = url.split('/');
-    var index = $.inArray('pwadmin', bases);
-    var base = bases[index + 1];
-    var aes = $("#list-group").find('a');
-    var targets = [];
-    for (var dex = 0; dex < aes.length; dex++) {
-        if (aes[dex].href.indexOf(base) !== -1) {
-            targets.push(aes[dex]);
-        }
-    }
-    var len = targets.length;
-    var target_a = targets[len - 1];
-    var target_li = $(target_a).parent('li');
-    target_a.style.color = 'white';
-    target_li.css('background-color', '#02a1c9');
-}
 
 //加载主类别id为main_label部分的代码
 function loadCategories(data) {
--- src/pwadmin/urls.py
+++ src/pwadmin/urls.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals, absolute_import
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from .views import (
-    pwmanager, index, task, accounts, statistic, permissions, contents, config, groups, fund
+    pwmanager, index, task, accounts, statistic, permissions, contents, config, assigns, fund
 )
 
 urlpatterns = [
@@ -31,23 +31,23 @@ urlpatterns = [
     url(r'^permissions/manager/$', permissions.ManagerList.as_view(), name='perm_manager'),
     url(r'^permissions/groups/$', permissions.GroupList.as_view(), name='perm_group'),
     url(r'^permissions/menu-tree/$', login_required(permissions.MenuTree.as_view()), name='perm_menu_tree'),
-    url(r'^label_manage/$', contents.LabelManage.as_view(), name='label_manage'),
+    url(r'^label_manage/$', assigns.LabelManage.as_view(), name='label_manage'),
     url(r'^label_dynamic/$', contents.LabelDynamic.as_view(), name='label_dynamic'),
     url(r'^report_dynamic/$', contents.ReportDynamic.as_view(), name='report_dynamic'),
     url(r'^submit_dynamic/$', contents.SubmitDynamic.as_view(), name='submit_dynamic'),
     url(r'^white_list/$', contents.WhiteList.as_view(), name='white_list'),
-    url(r'^group_manage/$', groups.GroupManage.as_view(), name='group_manage'),
-    url(r'^live_online/$', groups.LiveOnline.as_view(), name='live_online'),
-    url(r'^live_hide/$', groups.LiveHide.as_view(), name='live_hide'),
-    url(r'^live_search/$', groups.LiveSearch.as_view(), name='live_search'),
-    url(r'^live_special/$', groups.LiveSpecial.as_view(), name='live_special'),
-    url(r'^cover_verify/$', groups.CoverVerify.as_view(), name='cover_verify'),
-    url(r'^live_product/$', groups.LiveProduct.as_view(), name='live_product'),
-    url(r'^live_room_report/$', groups.LiveRoomReport.as_view(), name='live_room_report'),
-    url(r'^live_gift_report/$', groups.LiveGiftReport.as_view(), name='live_gift_report'),
-    url(r'^live_drama_report/$', groups.LiveDramaReport.as_view(), name='live_drama_report'),
-    url(r'^live_gift_income/$', groups.LiveGiftIncome.as_view(), name='live_gift_income'),
-    url(r'^label_rank_list/$', groups.LabelRankList.as_view(), name='label_rank_list'),
-    url(r'^sociaty_list/$', groups.SociatyList.as_view(), name='sociaty_list'),
-    url(r'^sociaty_leaguer_list/$', groups.SociatyLeaguerList.as_view(), name='sociaty_leaguer_list'),
+    url(r'^group_manage/$', assigns.GroupManage.as_view(), name='group_manage'),
+    url(r'^live_online/$', assigns.LiveOnline.as_view(), name='live_online'),
+    url(r'^live_hide/$', assigns.LiveHide.as_view(), name='live_hide'),
+    url(r'^live_search/$', assigns.LiveSearch.as_view(), name='live_search'),
+    url(r'^live_special/$', assigns.LiveSpecial.as_view(), name='live_special'),
+    url(r'^cover_verify/$', assigns.CoverVerify.as_view(), name='cover_verify'),
+    url(r'^live_product/$', assigns.LiveProduct.as_view(), name='live_product'),
+    url(r'^live_room_report/$', assigns.LiveRoomReport.as_view(), name='live_room_report'),
+    url(r'^live_gift_report/$', assigns.LiveGiftReport.as_view(), name='live_gift_report'),
+    url(r'^live_drama_report/$', assigns.LiveDramaReport.as_view(), name='live_drama_report'),
+    url(r'^live_gift_income/$', assigns.LiveGiftIncome.as_view(), name='live_gift_income'),
+    url(r'^label_rank_list/$', assigns.LabelRankList.as_view(), name='label_rank_list'),
+    url(r'^sociaty_list/$', assigns.SociatyList.as_view(), name='sociaty_list'),
+    url(r'^sociaty_leaguer_list/$', assigns.SociatyLeaguerList.as_view(), name='sociaty_leaguer_list'),
 ]
