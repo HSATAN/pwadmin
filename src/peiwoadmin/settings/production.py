552ec68f48cebc9f9d8fd96368c19ea3ebde1970
yanzhao <649038269@qq.com>
2017-11-22 12:18:09 +0800

    Update settings

--- src/peiwoadmin/settings/production.py
+++ src/peiwoadmin/settings/production.py
@@ -1,5 +1,25 @@
 from .base import *
+from peiwoadmin import secret
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['newadmin.raybo.com']
+SECRET_KEY = secret.SECRET_KEY
+
+DATABASES = {
+    'default': secret.DB_DEFAULT,
+    'test': secret.DB_TEST,
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'tmp', 'django_cache'),
+        'TIMEOUT': None,
+        'OPTIONS': {
+            'MAX_ENTRIES': 100 * 100 * 100
+        }
+    }
+}
+
+API_HOST = 'http://peiwo.admin.api:9090'
---
+++ src/peiwoadmin/settings/test.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from .base import *
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
+CORS_ORIGIN_ALLOW_ALL = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'peiwo_admin',
+        'USER': 'peiwo_admin',
+        "PASSWORD": '77WN88wwc',
+        "HOST": '172.16.10.133',
+
+    },
+    'test': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'peiwo',
+        'USER': 'peiwo',
+        "PASSWORD": '123456',
+        "HOST": '172.16.10.134',
+
+    },
+
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'tmp', 'django_cache'),
+        'TIMEOUT': None,
+        'OPTIONS': {
+            'MAX_ENTRIES': 100 * 100 * 100
+        }
+    }
+}
+
+API_HOST = 'http://172.16.10.134:9090'
+# API_HOST = 'http://10.10.100.58:9090'
